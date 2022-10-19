433ea4cc89d51fb7179eadf3edf704cc3d7a3c2f
zhaokai <zhaokai@bigo.sg>
2022-10-19 21:40:07 +0800

    init spring project

---
+++ src/main/kotlin/com/mind/base/DemoApplication.kt
@@ -0,0 +1,11 @@
+package com.mind.base
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MindBaseApplication
+
+fun main(args: Array<String>) {
+	runApplication<MindBaseApplication>(*args)
+}
---
+++ src/test/kotlin/com/mind/base/MindBaseTests.kt
@@ -0,0 +1,13 @@
+package com.mind.base
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MindBaseTests {
+
+	@Test
+	fun test() {
+	}
+
+}
