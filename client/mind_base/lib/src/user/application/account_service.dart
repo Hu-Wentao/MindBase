7f520e07c9ff26009b07047baa222c7c6936b97e
胡文涛 <35894003+Hu-Wentao@users.noreply.github.com>
2022-10-22 11:05:28 +0800

    feat team_service.dart

---
+++ client/mind_base/lib/src/user/application/account_service.dart
@@ -0,0 +1,38 @@
+part of 'service.dart';
+
+@lazySingleton
+class AccountService {
+  final cli.Account _actSrv;
+
+  AccountService(this._actSrv);
+
+  Future<Session> login({
+    required String email,
+    required String password,
+  }) {
+    return _actSrv.createEmailSession(email: email, password: password);
+  }
+
+  Future<User> signup({
+    String? name,
+    required String email,
+    required String password,
+    String userId = 'unique()',
+  }) =>
+      _actSrv.create(
+        userId: userId,
+        name: name,
+        email: email,
+        password: password,
+      );
+
+  Future logout([String? sessionId]) =>
+      _actSrv.deleteSession(sessionId: sessionId ?? 'current');
+
+  Future<User> getUser() async => await _actSrv.get();
+
+  Future<Session> getSession({String sessionId = 'current'}) =>
+      _actSrv.getSession(sessionId: sessionId);
+
+  Future<Session> createAnonymousSession() => _actSrv.createAnonymousSession();
+}
--- client/mind_base/lib/src/user/application/service.dart
+++ client/mind_base/lib/src/user/application/service.dart
@@ -1,41 +1,10 @@
-import 'package:appwrite/appwrite.dart' as ap;
+import 'package:appwrite/appwrite.dart' as cli;
+import 'package:dart_appwrite/dart_appwrite.dart' as srv;
 import 'package:get_arch_core/get_arch_core.dart';
+import 'package:mind_base/src/user/domain/entity.dart';
 
 import '../inter/dto.dart';
 
-@lazySingleton
-class AccountService {
-  final ap.Account _actSrv;
+part 'account_service.dart';
 
-  AccountService(this._actSrv);
-
-  Future<Session> login({
-    required String email,
-    required String password,
-  }) {
-    return _actSrv.createEmailSession(email: email, password: password);
-  }
-
-  Future<User> signup({
-    String? name,
-    required String email,
-    required String password,
-    String userId = 'unique()',
-  }) =>
-      _actSrv.create(
-        userId: userId,
-        name: name,
-        email: email,
-        password: password,
-      );
-
-  Future logout([String? sessionId]) =>
-      _actSrv.deleteSession(sessionId: sessionId ?? 'current');
-
-  Future<User> getUser() async => await _actSrv.get();
-
-  Future<Session> getSession({String sessionId = 'current'}) =>
-      _actSrv.getSession(sessionId: sessionId);
-
-  Future<Session> createAnonymousSession() => _actSrv.createAnonymousSession();
-}
+part 'team_service.dart';
