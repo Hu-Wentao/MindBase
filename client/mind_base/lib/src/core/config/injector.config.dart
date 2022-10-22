edeab13ef0fb66183a4709f209deae64d54cf916
胡文涛 <35894003+Hu-Wentao@users.noreply.github.com>
2022-10-22 11:10:50 +0800

    feat service.dart RealtimeSrv

--- client/mind_base/lib/src/core/config/injector.config.dart
+++ client/mind_base/lib/src/core/config/injector.config.dart
@@ -6,9 +6,11 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:appwrite/appwrite.dart' as _i4;
+import 'package:dart_appwrite/dart_appwrite.dart' as _i6;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
+import '../../app/application/service/service.dart' as _i5;
 import '../../user/application/service.dart'
     as _i3; // ignore_for_file: unnecessary_lambdas
 
@@ -26,5 +28,10 @@ _i1.GetIt $initGetIt(
   );
   gh.lazySingleton<_i3.AccountService>(
       () => _i3.AccountService(get<_i4.Account>()));
+  gh.lazySingleton<_i5.RealtimeSrv>(() => _i5.RealtimeSrv(get<dynamic>()));
+  gh.lazySingleton<_i3.TeamService>(() => _i3.TeamService(
+        get<_i4.Teams>(),
+        get<_i6.Teams>(),
+      ));
   return get;
 }
