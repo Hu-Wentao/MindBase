8483177cf99a57b79825bb4acadc4171a12fbce2
zhaokai <zhaokai@bigo.sg>
2022-10-22 12:47:42 +0800

    增加excel 数据导入导出功能

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.4"
-	id("io.spring.dependency-management") version "1.0.14.RELEASE"
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
+    id("org.springframework.boot") version "2.7.4"
+    id("io.spring.dependency-management") version "1.0.14.RELEASE"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
 }
 
 group = "com.mindbase"
@@ -12,28 +12,30 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("com.googlecode.aviator:aviator:5.3.2")
-	implementation("org.springframework.boot:spring-boot-starter-jdbc")
-	implementation("io.appwrite:sdk-for-kotlin:1.1.0")
-	implementation("mysql:mysql-connector-java:8.0.15")
-	implementation("com.alibaba:druid-spring-boot-starter:1.1.24")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.googlecode.aviator:aviator:5.3.2")
+    implementation("io.appwrite:sdk-for-kotlin:1.1.0")
+    implementation("mysql:mysql-connector-java:8.0.15")
+    implementation("com.alibaba:druid-spring-boot-starter:1.1.24")
+    implementation("org.apache.poi:poi-ooxml:5.2.3")
+    implementation("org.apache.commons:commons-lang3:3.12.0")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
--- server/src/main/kotlin/com/mind/base/service/ExplainDBServiceImpl.kt
+++ server/src/main/kotlin/com/mind/base/service/ExplainDBServiceImpl.kt
@@ -1,7 +1,6 @@
 package com.mind.base.service
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Service
 
@@ -10,7 +9,6 @@ import org.springframework.stereotype.Service
 class ExplainDBServiceImpl :ExplainDBService {
 
     @Autowired
-    @Qualifier("dbTest")
     private lateinit var jdbcTemplate: JdbcTemplate
 
     override fun queryAllFields(table: String): List<Map<String, Any>> {
