687dc6341aedb9a57787c3a74fa9fafc01710fa0
胡文涛 <35894003+Hu-Wentao@users.noreply.github.com>
2022-10-23 16:28:33 +0800

    fix ap_adapter.dart 修复权限管理数据转换

--- client/mind_base/lib/src/space/infra/ap_adapter.dart
+++ client/mind_base/lib/src/space/infra/ap_adapter.dart
@@ -1,5 +1,6 @@
 import 'package:appwrite/appwrite.dart';
 import 'package:dart_appwrite/models.dart' as srv;
+import 'package:get_sweet/get_sweet.dart';
 import 'package:mind_base/src/space/inter/dto/i_dto.dart';
 
 /// 适配 0.15.x > 1.0.x
@@ -29,18 +30,22 @@ class ApAdapter {
       ];
 
   static List<String> parsePermitToRead(srv.Collection collection) =>
-      (collection.$permissions as List<String>?)
-          ?.where((e) => e.startsWith(RegExp(r'read')))
-          .map((e) => e.replaceAllMapped(
-              RegExp(r'.+\(\"(.+)\"\)'), (m) => m.group(1) ?? ''))
-          .toList() ??
+      collection.$permissions
+          .takeIf<List>((_) => _.isNotEmpty)
+          ?.let<List, List<String>?>((it) => (it.map((e) => e as String))
+              .where((e) => e.startsWith(RegExp(r'read')))
+              .map((e) => e.replaceAllMapped(
+                  RegExp(r'.+\(\"(.+)\"\)'), (m) => m.group(1) ?? ''))
+              .toList()) ??
       [];
 
   static List<String> parsePermitToWrite(srv.Collection collection) =>
-      (collection.$permissions as List<String>?)
-          ?.where((e) => e.startsWith(RegExp(r'write|create|update|delete')))
-          .map((e) => e.replaceAllMapped(
-              RegExp(r'.+\(\"(.+)\"\)'), (m) => m.group(1) ?? ''))
-          .toList() ??
+      collection.$permissions
+          .takeIf<List>((_) => _.isNotEmpty)
+          ?.let<List, List<String>?>((it) => (it.map((e) => e as String))
+              .where((e) => e.startsWith(RegExp(r'write|create|update|delete')))
+              .map((e) => e.replaceAllMapped(
+                  RegExp(r'.+\(\"(.+)\"\)'), (m) => m.group(1) ?? ''))
+              .toList()) ??
       [];
 }
