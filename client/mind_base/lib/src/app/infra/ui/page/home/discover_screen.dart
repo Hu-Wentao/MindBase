5eaed470c0a003d62d4778f3833c75ed68cac58e
胡文涛 <35894003+Hu-Wentao@users.noreply.github.com>
2022-10-23 17:09:58 +0800

    feat discover_screen.dart 添加仿真效果

--- client/mind_base/lib/src/app/infra/ui/page/home/discover_screen.dart
+++ client/mind_base/lib/src/app/infra/ui/page/home/discover_screen.dart
@@ -4,8 +4,27 @@ class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const Text("发现");
-  }
+  Widget build(BuildContext context) => MainColumn.dense(
+        children: [
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                title: Text(
+                  "Google Financial",
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  child: Assets.images.discoverFake.image(
+                    fit: BoxFit.fitHeight,
+                    alignment: Alignment.centerLeft,
+                    height: 150,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      );
 }
-
